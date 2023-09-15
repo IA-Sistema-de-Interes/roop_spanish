49922ea91e9062e48f1c29c3b4b38c1e43a396da
IA(Sistema de Interes) <novedades2007@yahoo.com>
2023-09-15 14:10:24 -0300

    face_swapper.py

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -32,10 +32,7 @@ def clear_face_swapper() -> None:
     FACE_SWAPPER = None
 
 
-def pre_check() -> bool:
-    download_directory_path = resolve_relative_path('../models')
-    conditional_download(download_directory_path, ['https://huggingface.co/henryruhs/roop/resolve/main/inswapper_128.onnx'])
-    return True
+
 
 
 def pre_start() -> bool:
